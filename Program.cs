0f96c34bb6e8810d13e77b62ab15f16a065e555b
jasonstewartnz <jason.stewart@gmail.com>
2014-03-04 22:17:50 -0500

    ShapeFile created using SharpMap

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using SharpMap.Data.Providers;
+using NetTopologySuite;
+using GeoAPI;
+
+namespace UsStateVisualizer
+{
+	public class Program
+	{ 
+		public static void Main (string[] args)
+		{
+
+			GeoAPI.GeometryServiceProvider.Instance = new NtsGeometryServices ();
+
+			string DataDirectory = "/Users/jasonstewart/datasets/us states shapefile";
+			string shpFileName = DataDirectory + "/usa_state_shapefile.shp";
+
+			using (ShapeFile shapefile = new ShapeFile (shpFileName)) {
+				shapefile.Open ();
+
+				Console.WriteLine (shapefile.ToString ());
+			}
+
+			Console.WriteLine("Press any key to continue...");
+			Console.ReadKey();
+		}
+	}
+}
+
