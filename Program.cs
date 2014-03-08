ef1fdbe8798427ebb898c7d7d67287b614dc58ec
jasonstewartnz <jason.stewart@gmail.com>
2014-03-08 01:22:43 -0500

    Writes svg to console.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using SharpMap.Data.Providers;
 using NetTopologySuite;
+using System.Linq;
 using GeoAPI;
 using GeoAPI.Geometries;
 using System.Collections.ObjectModel;
@@ -17,6 +18,11 @@ namespace UsStateVisualizer
 			string DataDirectory = "/Users/jasonstewart/datasets/us states shapefile";
 			string shpFileName = DataDirectory + "/usa_state_shapefile.shp";
 
+
+			int svgHeight = 300;
+			int svgWidth = 500;
+
+
 			using (ShapeFile shapefile = new ShapeFile (shpFileName)) {
 				shapefile.Open ();
 
@@ -26,22 +32,42 @@ namespace UsStateVisualizer
 
 				Console.WriteLine ("Shapefile:");
 
+				Collection<string> svgPolygonList = new Collection<string>();
+					// Create polygons 	
+					foreach (IGeometry geometry in geometries) {
+						// Shape
+						var coords = geometry.Coordinates;
 
-				foreach (IGeometry geometry in geometries) {
-					// Shape
-					var coords = geometry.Coordinates;
-					Console.WriteLine ("Next coordinate set:");
+						Console.WriteLine ("Next coordinate set:");
 
-					foreach (Coordinate coord in coords){
+						var coordStrings = coords.Select( coord => string.Format ("{0},{1} ", coord.X, coord.Y) );
 
-						Console.WriteLine (coord.ToString());
+						double xAverage = coords.Average ( coord => coord.X );
+						double yAverage = coords.Average( coord => coord.Y );
+
+						string coordString = coordStrings.Aggregate ( (coordStringBase,coordStringNext) => coordStringBase + coordStringNext );
+
+						string svgPolygonHtml = string.Format("<polygon points={0} fill=\"red\"/>", coordString );
+
+						// title string, middle of polygon
+						string titleStringHtml = string.Format( "<text x=\"{0}\" y=\"{1}\" fill=\"red\">State number XX</text>", xAverage, yAverage );
+
+						string stateHtml = svgPolygonHtml + "\n" + titleStringHtml + "\n";
+
+						// Add state polygon, text to list 
+						svgPolygonList.Add (stateHtml);
 					}
-				}
+				
+					string statePolygonHtml = svgPolygonList.Aggregate((str1,str2) => str1 + str2);
+					string hgtWdtStr = string.Format ("height=\"{0}\" width=\"{1}\">", svgHeight, svgWidth);
+					
+					string svgHtml = "<svg " + hgtWdtStr + statePolygonHtml + "</svg>";
 
-				Console.WriteLine ("");
+				Console.WriteLine (svgHtml);
 			}
 
 			Console.WriteLine("Press any key to continue...");
+			Console.ReadKey();
 			Console.ReadKey();
 		}
 	}
